2392d26a43230ffaada0017c8fa222f4c7d951cf
Yulia Didan <ydidan2809@gmail.com>
2018-12-01 13:59:08 +0200

    adjustments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import $ from "jquery";
 
 $(function() {
+    google.maps.event.addDomListener(window, 'load', init);
+        
+    function init() {
+            var mapOptions = {
+            zoom: 11,
+            
+            center: new google.maps.LatLng(40.6700, -73.9400), 
+            
+            styles: [{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"simplified"},{"hue":"#ff0000"}]}]
+        };
+    
+        var mapElement = document.getElementById('map');
+    
+        var map = new google.maps.Map(mapElement, mapOptions);
+    
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(40.6700, -73.9400),
+            map: map,
+            title: 'Snazzy!'
+        });
+    }
+    
     
 });
 
@@ -8,24 +30,3 @@ $(function() {
 
 // $('.parallax-window').parallax({imageSrc: '/img/hero.jpg'});
 
-google.maps.event.addDomListener(window, 'load', init);
-        
-function init() {
-        var mapOptions = {
-        zoom: 11,
-        
-        center: new google.maps.LatLng(40.6700, -73.9400), 
-        
-        styles: [{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"simplified"},{"hue":"#ff0000"}]}]
-    };
-
-    var mapElement = document.getElementById('map');
-
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.6700, -73.9400),
-        map: map,
-        title: 'Snazzy!'
-    });
-}
